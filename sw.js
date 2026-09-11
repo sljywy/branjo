61cdfe2687232afe9e746310a07b2a6300380580
sljywy <77735800+sljywy@users.noreply.github.com>
2026-09-11 23:58:55 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE = 'gtifab-box-v2';
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/app.html', '/vehicles.js', '/manifest.json'])));
-  self.skipWaiting();
-});
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
