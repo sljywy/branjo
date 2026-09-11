1154abaa6f9f1355450b5f5f92cc6ccf2be8ac0d
sljywy <77735800+sljywy@users.noreply.github.com>
2026-09-11 15:40:25 +0200

    Test

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE = 'gtifab-box-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/app.html', '/vehicles.js', '/manifest.json'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
